e83c017b07e00bd89ab06c326c954f28b6fd2ebd
Ibe Augustine <aibe64@yahoo.com>
2025-10-24 18:36:01 +0100

    FIX

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // server: {
+  //   host: '0.0.0.0',
+  //   port: 5001,
+  //   strictPort: true
+  // }
   server: {
-    host: '0.0.0.0',
-    port: 5001,
-    strictPort: true
+    port: 3000,
+    proxy: {
+      // Proxy API requests to your backend
+      '/api': {
+        target: 'https://unmercerized-garnet-unridiculous.ngrok-free.dev', // your backend URL
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
